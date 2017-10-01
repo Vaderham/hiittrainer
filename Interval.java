c15bb8427d47636a4abccaf6b722a181b626e259
Reagan Harper <reaganharper@Reagans-MacBook-Pro.local>
2017-10-02 12:28:44 +1300

    Updated Pausanle timer with Sam's suggestions. Starting to change buttons over to material design with FAB's etc

---
+++ Interval.java
@@ -0,0 +1,23 @@
+package com.example.reaganharper.hiittrainer02;
+
+import java.io.Serializable;
+
+public class Interval implements Serializable{
+
+    private String name;
+    private long intervalTime;
+
+    public Interval(String name, long intervalTime) {
+        this.name = name;
+        this.intervalTime = intervalTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getIntervalTime() {
+        return intervalTime;
+    }
+
+}
